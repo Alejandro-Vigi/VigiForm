3080cde87319f14ab816d89cdc1c8b80fb86eb8a
Alejandro Vigi <marco.vigi@ingenieria.unam.edu>
2025-03-17 13:51:46 -0600

    Iniciando navegacion tipo burger bottom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+    // Agregando dependencia de navegacion para compose
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/example/vigiform/navigation/Routes.kt
@@ -0,0 +1,11 @@
+package com.example.vigiform.navigation
+
+sealed class Routes (
+    val route: String
+) {
+    data object LeyOhm: Routes("leyOhm")
+    data object EnergiaCinetica: Routes("energiaCinetica")
+    data object Ensilar: Routes("ensilar")
+    data object Densidad: Routes("densidad")
+
+}
